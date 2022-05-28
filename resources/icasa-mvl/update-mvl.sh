42a5fafbc8d04b7c942c2a6c92bfa38ca4d790d3
Christopher Villalobos <cvillalobos@ufl.edu>
2022-05-27 21:17:38 -0400

    Import icasa-mvl resource project
    * Use the icasa-mvl resource project until a better solution is available.

--- resources/icasa-mvl/update-mvl.sh
+++ resources/icasa-mvl/update-mvl.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-curl -o src/main/resources/icasa-mvl/pathfinder.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=0"
-curl -o src/main/resources/icasa-mvl/management_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=2"
-curl -o src/main/resources/icasa-mvl/crop_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=4"
-curl -o src/main/resources/icasa-mvl/metadata_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=5"
-curl -o src/main/resources/icasa-mvl/other_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=6"
-curl -o src/main/resources/icasa-mvl/obs_pathfinder.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=8"
-curl -o src/main/resources/icasa-mvl/metadata_filter.csv "https://docs.google.com/spreadsheets/d/1kngp8bYjFayGtUO4mS7BsxXX8__FG6qlN6UV1a8daog/pub?output=csv&gid=0"
+# Pathfinding files
+curl -sLo src/main/resources/icasa-mvl/metadata.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=557875855"
+curl -sLo src/main/resources/icasa-mvl/management_info.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=0"
+curl -sLo src/main/resources/icasa-mvl/soils_data.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=250742177"
+curl -sLo src/main/resources/icasa-mvl/weather_data.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=2126041133"
+curl -sLo src/main/resources/icasa-mvl/measured_data.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=8"
+# Code files
+curl -sLo src/main/resources/icasa-mvl/metadata_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=5"
+curl -sLo src/main/resources/icasa-mvl/crop_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=4"
+curl -sLo src/main/resources/icasa-mvl/management_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=2"
+curl -sLo src/main/resources/icasa-mvl/pest_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=7"
+curl -sLo src/main/resources/icasa-mvl/other_codes.csv "https://docs.google.com/spreadsheets/d/1MYx1ukUsCAM1pcixbVQSu49NU-LfXg-Dtt-ncLBzGAM/pub?output=csv&gid=6"
+
